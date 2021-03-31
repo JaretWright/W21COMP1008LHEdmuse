7b0ab211d0637838cd437a6cc6209805a9e87b8d
Jaret Wright <hkkA223>
2021-03-31 12:48:12 -0400

    Added methods to return an ArrayList of Student objects and another to return an ArrayList of Professor objects.

--- src/controllers/DashboardViewController.java
+++ src/controllers/DashboardViewController.java
@@ -1,4 +1,42 @@
 package controllers;
 
-public class DashboardViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import models.Person;
+import models.Professor;
+import models.Student;
+import utilities.DBUtility;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class DashboardViewController implements Initializable {
+
+    @FXML
+    private Label studentsLabel;
+
+    @FXML
+    private ListView<Student> studentsListView;
+
+    @FXML
+    private Label professorsLabel;
+
+    @FXML
+    private ListView<Professor> professorsListView;
+
+    @FXML
+    private Label coursesLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        studentsListView.getItems().addAll(DBUtility.getStudentsFromDB());
+        DBUtility.getProfessorsFromDB();
+    }
+
+//    @FXML
+//    private ListView<?> coursesListView;
+
 }
